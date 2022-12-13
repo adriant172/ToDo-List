bebc0eba2e466fd00eebdac79fd32bc00750a445
Matthew <matthew.thomas@jp.kpmg.com>
2022-12-13 23:36:10 +0900

    add foundational structure/styling for app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+import { generateMainLayout } from "./main-page";
+import './styles.css';
+
+const content = document.querySelector("#content");
+
+generateMainLayout(content);
+
+
+
 class toDoItem {
-    constructor(title, description, dueDate, priorityLevel, notes, checklist){
+    constructor(title, description, dueDate, priorityLevel, notes, status, checklist){
         this.title = title;
         this.description= description;
         this.dueDate = dueDate;
         this.priorityLevel = priorityLevel;
         this.notes = notes;
         this.checklist = checklist;
+        this.status = status;
     }
     
 }
+
